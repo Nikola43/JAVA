cfa7ae00645cc41b578d8838a614abaa4ac76278
Paulo Soares <pauloxti@gmail.com>
2017-10-31 13:30:06 +0100

    Probando autocompletado atom
    Configurando autocompletado java atom

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -1,7 +1,16 @@
 public class HelloWorld
+
+import java.beans.MetaData;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 {
     public static void main(String[] args)
     {
         System.out.println("Hello");
+        ArrayList<String> arrayList = new ArrayList();
+        arrayList.add("gg");
+
+        Scanner sc = new Scanner(System.in);
     }
 }
