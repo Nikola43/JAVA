0ad2e2570ed24dc91f21fbedf223d038a180b557
Paulo Soares <pauloxti@gmail.com>
2018-05-08 10:09:23 +0200

    ficheros

---
+++ Ficheros/Main.java
@@ -0,0 +1,38 @@
+/*
+1. Crea una función a la que se le pase como parámetro una cadena, y genere un fichero de
+texto con el nombre de dicha cadena. La función debe rellenar un fichero con cadenas (en
+diferentes líneas) hasta que el usuario escriba la palabra “final”.
+
+2. Crea un función a la que se le pase como parámetro un número num y genere un fichero que
+guarde los num primeros números primos (empezando desde el 2) en dicho fichero. La
+función devuelve true si ha terminado de manera correcta y false en caso contrario, y
+además pide al usuario el nombre que quiere ponerle al fichero.
+
+3. Crea una función que imprima por consola cualquier fichero generado con la función
+anterior.
+
+4. Crea una función a la que se le pase como parámetro un número y elimine de un fichero que
+tenga números primos dicho número. Ten en cuenta, que para hacerlo, necesitarás de un
+fichero temporal.
+
+5. Crea una función que cree un fichero con los siguientes datos de clientes: Nombre, Apellido,
+Edad. La función pregunta cómo se tiene que llamar el fichero, y pide los datos hasta que el
+usuario inserta la palabra Fin (en el primer campo que pide). La función devuelve true si ha
+terminado de manera correcta y false en caso contrario, y además pide al usuario el nombre
+que quiere ponerle al fichero.
+
+Vamos a proceder a rellenar el fichero con los siguientes valores:
+Juan    Palomez  35
+Paloma  Sancho   19
+Arturo  Coy      40
+Noelia  Bravo    33
+Pancho  Cancho   21
+
+6. Crea una funcion mostrarClientes para que imprima por pantalla todos los registros de los
+ficheros que tengan la estructura del fichero anterior.
+
+7. La siguiente función deberá crear dos ficheros de clientes a partir de un fichero de clientes
+pasado como parámetro. El programa meterá los datos en uno u otro fichero en función de si
+la edad es menor o igual que la dada por el usuario o mayor que ésta respectivamente.
+
+*/
